61cef21ef2897d28c2d0c3a895b1a2c6a1735e9d
boligalausha-cloud <boligalausha@gmail.com>
2026-08-28 11:13:39 +0530

    Fix formatting and case in HelloWorld output

--- TNSIF_JAVA/src/org/tnsif/acce/c2tc/java/HelloWorld.java
+++ TNSIF_JAVA/src/org/tnsif/acce/c2tc/java/HelloWorld.java
@@ -3,6 +3,6 @@ package org.tnsif.acce.c2tc.java;
 public class HelloWorld {
 
 public static void main(String[]args) {
-	System.out.println("Hello world");
+	System.out.println("Hello World");
 }
 }
