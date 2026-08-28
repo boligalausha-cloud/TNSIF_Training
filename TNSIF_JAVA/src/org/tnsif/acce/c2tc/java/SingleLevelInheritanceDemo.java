5cbf56712818dcc8c9c22858939a8c5d2724be0b
boligalausha-cloud <boligalausha@gmail.com>
2026-08-28 12:05:13 +0530

    Add SingleLevelInheritanceDemo with Animal and Dog classes

---
+++ TNSIF_JAVA/src/org/tnsif/acce/c2tc/java/SingleLevelInheritanceDemo.java
@@ -0,0 +1,23 @@
+package org.tnsif.acce.c2tc.java;
+
+// Parent class
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+// Child class - Single Inheritance
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class SingleLevelInheritanceDemo {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();  // from parent
+        d.bark(); // from child
+    }
+}
