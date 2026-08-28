9ad29ed57cd39a216009ce0b2954df5ca5db096d
boligalausha-cloud <boligalausha@gmail.com>
2026-08-28 11:59:39 +0530

    Add IntegerToRoman class for integer to Roman conversion

---
+++ TNSIF_JAVA/src/org/tnsif/acce/c2tc/java/IntegerToRoman.java
@@ -0,0 +1,19 @@
+package org.tnsif.acce.c2tc.java;
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder roman = new StringBuilder();
+        for(int i=0;i<values.length;i++){
+            while(num>=values[i]){
+                roman.append(symbols[i]);
+                num-=values[i];
+            }
+        }
+        return roman.toString();
+    }
+    public static void main(String[] args){
+        IntegerToRoman obj = new IntegerToRoman();
+        System.out.println(obj.intToRoman(1994));
+    }
+}
