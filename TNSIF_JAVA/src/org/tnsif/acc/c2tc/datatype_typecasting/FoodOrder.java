004b8aebd06d639f879fb815b9f165dbd3334d21
boligalausha-cloud <boligalausha@gmail.com>
2026-09-03 21:28:07 +0530

    Create FoodOrder class with data type examples
    Added a FoodOrder class with main method demonstrating primitive and non-primitive data types.

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/datatype_typecasting/FoodOrder.java
@@ -0,0 +1,27 @@
+package org.tnsif.acc.c2tc.datatype_typecasting;
+
+public class FoodOrder {
+
+	public static void main(String[] args) {
+		
+		//Primitive datatype
+		
+		int     orderId =101;
+		double  price=299.99;
+		boolean isDelivered= false;
+		char    rating='A';
+		float   deliveryDistance=5.3f;
+		long    deliveryBoyPhone= 9835365252L;
+		byte    deliverTime=45;
+		short   restaurantId=12;
+		
+		
+		//Non  Primitive datatype
+		
+		String CustomerName="Anajli";
+		String[] foodItem= {"Burger" , "Fries","Coke"};
+		
+		
+	}
+
+}
