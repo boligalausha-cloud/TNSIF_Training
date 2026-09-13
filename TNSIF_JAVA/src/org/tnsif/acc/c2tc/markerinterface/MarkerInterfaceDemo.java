741e1d57f209e6d18cbc5744f955608a64d58483
boligalausha-cloud <boligalausha@gmail.com>
2026-09-13 20:11:22 +0530

    Refactor MarkerInterfaceDemo and remove Student class

--- TNSIF_JAVA/src/org/tnsif/acc/c2tc/markerinterface/MarkerInterfaceDemo.java
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/markerinterface/MarkerInterfaceDemo.java
@@ -1,48 +1,24 @@
 package org.tnsif.acc.c2tc.markerinterface;
 
-public class Student implements Registerable
-{
-	private int id;
-    private String name;
-    private double fees;
-    private String course;
-    
-    Student(int id,String name,double fees, String course)
-    {
-    	     this.id=id;
-    	     this.name=name;
-    	     this.fees=fees;
-    	     this.course=course;
-    	     
-    }
-    
-    public void setId(int id) {
-		this.id = id;
+public class MarkerInterfaceDemo {
+
+	public static void main(String[] args) {
+		Registerable s=new Student(12,"Sushma",20000,"Java Programming");
+		Registerable s1=new Student(13,"Anil",30000,"Java Programming");
+		
+		Object obj=new Object();
+		
+		if(obj instanceof Registerable)
+		{
+			System.out.println("Student is registered for this course");
+			
+		}
+		else
+		{
+			System.out.println("Student is not registered for this course");
+		}
+		
+
 	}
-     public int getId() {
-		return id;
-	}
-	
-     public void setName(String name) {
- 		this.name = name;
- 	}
-     
-	public String getName() {
-		return name;
-	}
-	
-	public void setFees(double fees) {
-		this.fees = fees;
-	}
-	public double getFees() {
-		return fees;
-	}
-	public void setCourse(String course) {
-		this.course = course;
-	}
-	
-	public String getCourse() {
-		return course;
-	}
-	    
+
 }
